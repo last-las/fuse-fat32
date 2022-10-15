a43366642cc60f3ec4cf1c5ac23c50bd6cbb1b62
las <478942543@qq.com>
2022-10-15 17:26:52 +0800

    feat: init test cases for fat32 system call

--- test/test_system_call.cpp
+++ test/test_system_call.cpp
@@ -1,12 +1,79 @@
 #include "gtest/gtest.h"
 
-TEST(normal, success) {
-    EXPECT_EQ(1, 1);
+TEST(OpenTest, NonExistFile) {
 }
 
-TEST(normal, failed) {
-    EXPECT_EQ(1, 2);
+TEST(OpenTest, ExistFile) {
 }
+
+TEST(OpenTest, ExistDir) {
+}
+
+TEST(OpenTest, LongName) {
+}
+
+TEST(OpenTest, MultipleDir) {
+}
+
+TEST(FstatTest, Fat32ConstantValue) {
+}
+
+TEST(FstatTest, ChangeTime) {
+}
+
+TEST(MkdirTest, CrtSimpleDir) {
+}
+
+TEST(MkdirTest, CrtExistDir) {
+}
+
+TEST(MkdirTest, CrtMultipleLevelDir) {
+}
+
+TEST(MkdirTest, ConstantMode) {
+}
+
+TEST(UnlinkTest, NonExist) {
+}
+
+TEST(UnlinkTest, RmFile) {
+}
+
+TEST(UnlinkTest, RmDir) {
+}
+
+TEST(RmdirTest, RmDir) {
+}
+
+TEST(RenameTest, IgnoreFlags) {
+}
+
+TEST(RenameTest, SrcNonExist) {
+}
+
+TEST(RenameTest, SrcFileDstNonExist) {
+}
+
+TEST(RenameTest, SrcDirDstNonExist) {
+}
+
+TEST(RenameTest, SrcFileDstFile) {
+}
+
+TEST(RenameTest, SrcFileDstDir) {
+}
+
+TEST(RenameTest, SrcDirDstFile) {
+}
+
+TEST(RenameTest, SrcDirDstDirEmpty) {
+}
+
+TEST(RenameTest, SrcDirDstDirNonEmpty) {
+}
+
+
+// TODO: remove all created test files.
 
 int main(int argc, char **argv) {
     testing::InitGoogleTest(&argc, argv);
