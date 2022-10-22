600d486cf0b1d18c508bf09d887882e8b484b78d
las <478942543@qq.com>
2022-10-22 18:13:29 +0800

    feat: implement LRUCache

--- library/util.h
+++ library/util.h
@@ -18,14 +18,33 @@ namespace util {
     public:
         explicit LRUCache(u32 max_size) noexcept: max_size_{max_size} {}
 
-        void put(key_t key, value_t value) noexcept {}
+        void put(key_t key, value_t value) noexcept {
+            if (caches_map_.find(key) != caches_map_.end()) {
+                caches_map_.erase(key);
+                key_queue_.remove(key);
+            }
+            caches_map_.insert(key, value);
+            key_queue_.push_back(key);
 
-        std::optional<value_t> get(key_t key) noexcept {}
+            if (caches_map_.size() > max_size_) {
+                auto removed_key = key_queue_.begin();
+                caches_map_.erase(*removed_key);
+                key_queue_.pop_front();
+            }
+        }
+
+        std::optional<value_t> get(key_t key) noexcept {
+            if (caches_map_.find(key) != caches_map_.end()) {
+                return std::optional(caches_map_[key]);
+            } else {
+                return std::nullopt;
+            }
+        }
 
     private:
         u32 max_size_;
-        std::unordered_map<key_t, std::pair<u32, value_t>> caches_map_;
-        std::list<key_t> fifo_queue_;
+        std::unordered_map<key_t, value_t> caches_map_;
+        std::list<key_t> key_queue_;
     };
 } // namespace util
 
